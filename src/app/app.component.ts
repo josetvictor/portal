3a7bb47d73666b3f1973356ac8eee9135b2d2a44
Jose Victor <zevicctor2@gmail.com>
2024-07-21 22:27:25 -0300

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    AboutComponent,
+    BlogComponent,
+    PageNotFoundComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(public router: Router) {}
   title = 'portal';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'/main', pathMatch: 'full'},
+    {path: 'main', component: MainComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: 'resume', component: ResumeComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
